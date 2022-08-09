f7ac56d49504e88b1cc3c9ab9d58005f7da9843d
Cwarrick10 <warrickchar@gmail.com>
2022-08-09 12:11:35 -0400

    updown code

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,30 @@
 package io.zipcoder;
-
+import java.util.HashMap;
 public class Music {
 
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        String selectUp = playList[startIndex];
+        String seledtDown = playList[startIndex];
+        int upIndex = startIndex;
+        int downIndex = startIndex;
+        int presses = 0;
+        int lastIndex = this.playList.length - 1;
+
+        while (!selectUp.equals(selection) && !seledtDown.equals(selection)) {
+            upIndex = (upIndex == 0) ? lastIndex : upIndex - 1;
+            downIndex = (downIndex == lastIndex) ? 0 : downIndex + 1;
+            presses++;
+            selectUp = playList[upIndex];
+            seledtDown = playList[downIndex];
+        }
+
+        return presses;
     }
 }
